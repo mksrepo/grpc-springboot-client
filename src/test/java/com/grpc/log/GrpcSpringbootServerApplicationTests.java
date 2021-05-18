77971e686e330ae3cda5e92ab7e03751855ad1a4
mrinmaysantra <mrinmay.santra@gmail.com>
2021-05-18 18:46:25 +0530

    Initial commit

---
+++ src/test/java/com/grpc/log/GrpcSpringbootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grpc.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpringbootServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
